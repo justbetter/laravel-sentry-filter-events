0ca8facc2d36d34691f71adbea0893c896e44fd0
Jade <jade@justbetter.nl>
2026-07-09 09:51:47 +0200

    Simplify rector config

--- rector.php
+++ rector.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -12,10 +10,6 @@ return RectorConfig::configure()
         __DIR__.'/tests',
     ])
     ->withPhpSets()
-    ->withSkip([
-        PostIncDecToPreIncDecRector::class,
-        AddSeeTestAnnotationRector::class,
-    ])
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
@@ -25,6 +19,5 @@ return RectorConfig::configure()
         instanceOf: true,
         earlyReturn: true,
         carbon: true,
-        rectorPreset: true,
         phpunitCodeQuality: true,
     );
